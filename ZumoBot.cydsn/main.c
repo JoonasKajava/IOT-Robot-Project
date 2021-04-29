329dd2e7120b11b645fce85646f8f48eeb200bbb
Joonas Kajava <me@joonaskajava.com>
2021-04-29 13:27:37 +0300

    sumo wrestling

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -47,6 +47,7 @@
 #include <sys/time.h>
 #include "serial1.h"
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * @file    main.c
  * @brief   
@@ -54,16 +55,112 @@
 */
 
 #if 1
+#define black true
+#define white false
+
+struct sensors_ dig;
+int speed = 140;
+
+void init();
+void findNext(bool color);
+void printReflectance();
+void stayInside();
+void avoidEdge();
+void avoidObstacle();
+
+void tankTurn();
 // Hello World!
 void zmain(void)
 {
-    printf("\nHello, World!\n");
+    init();
 
-    while(true)
+    // Drive to the edge of the ring
+    findNext(black);
+    print_mqtt("Zumo4/ready", "%s", "zumo");
+    IR_wait();
+    int start = xTaskGetTickCount();
+    print_mqtt("Zumo4/start", "%d", start);
+    // Enter the ring
+    findNext(white);
+
+    stayInside();
+    motor_forward(0,0);
+    motor_stop();
+    int stop = xTaskGetTickCount();
+    print_mqtt("Zumo4/stop", "%d", stop);
+    print_mqtt("Zumo4/time", "%d", stop - start);
+    while (true)
     {
         vTaskDelay(100); // sleep (in an infinite loop)
     }
- }   
+}
+
+void init()
+{
+    srand(time(NULL));
+    motor_start();
+    motor_forward(0, 0);
+    IR_Start();
+    Ultra_Start();
+    reflectance_start();
+    reflectance_set_threshold(16000, 16000, 16000, 16000, 16000, 16000);
+}
+
+// white = false, black = true
+void findNext(bool color)
+{
+    reflectance_digital(&dig);
+    motor_forward(speed, 1);
+    while (dig.L3 != color || dig.R3 != color)
+        reflectance_digital(&dig);
+    motor_forward(0, 0);
+}
+
+void stayInside()
+{
+    while (SW1_Read())
+    {
+        motor_forward(speed, 1);
+        avoidEdge();
+        avoidObstacle();
+    }
+}
+
+void avoidEdge()
+{
+    reflectance_digital(&dig);
+    if (dig.L3 || dig.R3)
+    {
+        motor_backward(speed, 10);
+        tankTurn();
+    }
+}
+
+void avoidObstacle()
+{
+    if (Ultra_GetDistance() < 5)
+    {
+        print_mqtt("Zumo4/obstacle", "%d", xTaskGetTickCount());
+        tankTurn();
+    }
+}
+
+void tankTurn()
+{
+    int random = rand() % 300 + 100;
+    SetMotors(1, 0, speed, speed, random);
+}
+
+void printReflectance()
+{
+    struct sensors_ raw;
+    struct sensors_ ref;
+    reflectance_read(&raw);
+    reflectance_digital(&ref);
+    printf("\n\nRAW: L(3,2,1) = {%d, %d, %d}, R(1,2,3) = {%d, %d, %d}\n", raw.L3, raw.L2, raw.L1, raw.R1, raw.R2, raw.R3);
+    printf("DIG: L(3,2,1) = {%d, %d, %d}, R(1,2,3) = {%d, %d, %d}\n", ref.L3, ref.L2, ref.L1, ref.R1, ref.R2, ref.R3);
+}
+
 #endif
 
 #if 0
@@ -90,9 +187,8 @@ void zmain(void)
         BatteryLed_Write(!SW1_Read());
         vTaskDelay(100);
     }
- }   
+ }
 #endif
-
 
 #if 0
 //battery level//
@@ -126,7 +222,7 @@ void zmain(void)
         }
         vTaskDelay(500);
     }
- }   
+ }
 #endif
 
 #if 0 
@@ -205,9 +301,8 @@ void zmain(void)
             while(SW1_Read() == 0) vTaskDelay(10); // wait while button is being pressed
         }        
     }
- }   
+ }
 #endif
-
 
 #if 0
 //ultrasonic sensor//
@@ -221,7 +316,7 @@ void zmain(void)
         printf("distance = %d\r\n", d);
         vTaskDelay(200);
     }
-}   
+}
 #endif
 
 #if 0
@@ -245,10 +340,8 @@ void zmain(void)
         if(led) printf("Led is ON\n");
         else printf("Led is OFF\n");
     }    
- }   
+ }
 #endif
-
-
 
 #if 0
 //IR receiver - read raw data
@@ -277,9 +370,8 @@ void zmain(void)
             printf("%d %d\r\n",b, l);
         }
     }    
- }   
+ }
 #endif
-
 
 #if 0
 //reflectance
@@ -307,9 +399,8 @@ void zmain(void)
         
         vTaskDelay(200);
     }
-}   
+}
 #endif
-
 
 #if 0
 //motor
@@ -358,8 +449,8 @@ void zmain(void)
         printf("%8d %8d %8d\n",data.accX, data.accY, data.accZ);
         vTaskDelay(50);
     }
- }   
-#endif    
+ }
+#endif
 
 #if 0
 // MQTT test
@@ -381,9 +472,8 @@ void zmain(void)
         vTaskDelay(1000);
         ctr++;
     }
- }   
+ }
 #endif
-
 
 #if 0
 void zmain(void)
@@ -421,7 +511,7 @@ void zmain(void)
         }
         vTaskDelay(10);
     }
- }   
+ }
 
 #endif
 
@@ -457,7 +547,7 @@ void zmain(void)
         }
         vTaskDelay(50);
     }
- }   
+ }
 #endif
 
 /* [] END OF FILE */
